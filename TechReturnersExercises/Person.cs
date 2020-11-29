3b30b98f557b40fdbbda616f8cee837fe26b2a4c
Kibeida <Kibeida@yahoo.com>
2020-11-29 22:15:45 +0000

    Exercise002 complete

---
+++ TechReturnersExercises/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TechReturnersExercises
+{
+    public class Person
+    {
+        public Person(string firstName,string lastName,string city,int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            Age = age;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+    }
+}
