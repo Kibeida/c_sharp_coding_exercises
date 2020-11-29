59b8af44f0505d10a5c53c5d15d1e72f1e2b3697
Kibeida <Kibeida@yahoo.com>
2020-11-29 22:13:32 +0000

    Exercise001 complete

--- TechReturnersExercises/Exercise001.cs
+++ TechReturnersExercises/Exercise001.cs
@@ -8,31 +8,52 @@ namespace TechReturners.Exercises
         public static String CapitalizeWord(String word) 
         {
             // Add your code here!
+            return (char.ToUpper(word[0]) + word.Substring(1));
+
             throw new NotImplementedException();
         }
 
         public static String GenerateInitials(String firstName, String lastName) 
         {
             // Add your code here!
-            throw new NotImplementedException();
+
+            char fName = char.ToUpper(firstName[0]);
+            char lName = char.ToUpper(lastName[0]);
+
+            return $"{fName}.{lName}";
+             
         }
 
         public static double AddVat(double originalPrice, double vatRate) 
         {
             // Add your code here!
-            throw new NotImplementedException();
+            double vat = originalPrice * vatRate / 100;
+            double result = originalPrice + vat;
+            
+            return Math.Round(result,2);
+           
         }
 
         public static String Reverse(String sentence) 
         {
             // Add your code here!
-            throw new NotImplementedException();
+            var charArray = sentence.ToCharArray();
+            Array.Reverse(charArray);
+            var result = new String(charArray);
+            return result;
         }
 
         public static int CountLinuxUsers(List<User> users) 
         {
             // Add your code here!
-            throw new NotImplementedException();
+            int result = 0;
+
+            foreach (var user in users)
+            {
+                if (user.Type == "Linux") result += 1;
+            }
+
+            return result;
         }
     }
 }
