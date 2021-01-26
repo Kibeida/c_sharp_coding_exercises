79a14a8980eda6234e39f79871652f10064fa282
Kibeida <Kibeida@yahoo.com>
2021-01-26 01:10:12 +0000

    Code refactoring

--- TechReturnersExercises/Exercise002.cs
+++ TechReturnersExercises/Exercise002.cs
@@ -9,20 +9,12 @@ namespace TechReturners.Exercises
         {
             return person.City == "Manchester";
         }
-        // {
-
-        //     bool result = person.City == "Manchester" ? true : false;
-
-        //     return result;
-        // }
-
 
         public static bool CanWatchFilm(Person person, int ageLimit)
         {
+            return person.Age >= ageLimit;
 
-            bool result = person.Age >= ageLimit ? true : false;
-
-            return result;
         }
     }
+
 }
